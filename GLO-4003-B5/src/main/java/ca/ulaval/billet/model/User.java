88e21a08a2c4a3fdf6bad9385dd63e1ecc961498
bensport0007 <bensport0007@gmail.com>
2013-09-20 14:01:58 -0400

    Adding User class

---
+++ GLO-4003-B5/src/main/java/ca/ulaval/billet/model/User.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ * @author Ben
+ *	La javadoc, la javadoc !
+ */
+
+package ca.ulaval.billet.model;
+
+public class User {
+
+	private String username;
+	private String password;
+	private String firstName;
+	private String LastName;
+	private String email;
+	private String accessLevel;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return LastName;
+	}
+	
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getAccessLevel() {
+		return accessLevel;
+	}
+	
+	public void setAccessLevel(String accessLevel) {
+		this.accessLevel = accessLevel;
+	}
+
+	
+	
+}
