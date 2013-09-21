79d6de1cd8d74e61162ea98ba42bb614b6ed1483
carlo boutet <carlo_boutet@hotmail.com>
2013-09-21 13:13:05 -0400

    classes de base pour read/write xml

---
+++ GLO-4003-B5/src/main/java/ca/ulaval/billet/dataUtil/XmlReader.java
@@ -0,0 +1,22 @@
+package ca.ulaval.billet.dataUtil;
+
+public class XmlReader {
+	public static final String DATA_FILE= "src/main/resources/TestDataProjetUniversite.xml";
+
+	public XmlReader(){
+		
+	}
+	
+	private boolean connect(){
+		return false;
+	}
+	
+	private boolean disconnect(){
+		return false;
+	}
+	
+	public static void main(String [] args)
+	{
+	
+	}
+}
---
+++ GLO-4003-B5/src/main/java/ca/ulaval/billet/dataUtil/XmlWriter.java
@@ -0,0 +1,23 @@
+package ca.ulaval.billet.dataUtil;
+
+public class XmlWriter {
+public static final String DATA_FILE= "src/main/resources/TestDataProjetUniversite.xml";
+
+	public XmlWriter(){
+		
+	}
+	
+	private boolean connect(){
+		return false;
+	}
+	
+	private boolean disconnect(){
+		return false;
+	}
+	
+	public static void main(String [] args)
+	{
+	
+	}
+	
+}
