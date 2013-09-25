893d8a9cf138610c6ddd67c6cc2006e34b5dabd6
Kar0t <perreault.simon@hotmail.com>
2013-09-25 01:57:47 -0400

    Link the add ticket  to the xml file writer

--- GLO-4003-B5/src/main/java/ca/ulaval/ticketmaster/web/converter/TicketConverter.java
+++ GLO-4003-B5/src/main/java/ca/ulaval/ticketmaster/web/converter/TicketConverter.java
@@ -1,28 +1,31 @@
 package ca.ulaval.ticketmaster.web.converter;
 
+import ca.ulaval.ticketmaster.dao.util.DataManager;
 import ca.ulaval.ticketmaster.model.Ticket;
 import ca.ulaval.ticketmaster.web.viewmodels.TicketViewModel;
 
 public class TicketConverter {
 
-	static public Ticket convert(TicketViewModel viewModel) {
-		Ticket entry = new Ticket(2); // TODO Generate random id (Passing data manager)
+	static public Ticket convert(TicketViewModel viewModel, DataManager datamanager) {
+		Ticket entry = new Ticket(12); // TODO Generate random id (Passing data manager)
 		entry.setSection(viewModel.getSection());
 		entry.setSeat(viewModel.getSeat());
 		entry.setOwner(viewModel.getOwner());
 		entry.setPrice(viewModel.getPrice());
 		entry.setResellprice(viewModel.getResellprice());
+		entry.setEvent(viewModel.getEvent());
 		
 		return entry;
 	}
 	
 	static public TicketViewModel convert(Ticket entry) {
-		TicketViewModel viewModel = new TicketViewModel(); 
+		TicketViewModel viewModel = new TicketViewModel(entry.getEvent()); 
 		viewModel.setSection(entry.getSection());
 		viewModel.setSeat(entry.getSeat());
 		viewModel.setOwner(entry.getOwner());
 		viewModel.setPrice(entry.getPrice());
 		viewModel.setResellprice(entry.getResellprice());
+		viewModel.setEvent(entry.getEvent());
 		
 		return viewModel;
 	}
