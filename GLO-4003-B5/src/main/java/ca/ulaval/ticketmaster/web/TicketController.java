1a1fa9f9eb158b8c48b420ba4768a67efda77c59
Kar0t <perreault.simon@hotmail.com>
2013-09-26 00:36:36 -0400

    You can now add Event to the list  care with the date formating... :S

--- GLO-4003-B5/src/main/java/ca/ulaval/ticketmaster/web/TicketController.java
+++ GLO-4003-B5/src/main/java/ca/ulaval/ticketmaster/web/TicketController.java
@@ -7,11 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ca.ulaval.ticketmaster.dao.util.DataManager;
 import ca.ulaval.ticketmaster.model.Event;
@@ -43,16 +41,16 @@ public class TicketController {
 
 	@RequestMapping(value = "/add/{eventId}", method = RequestMethod.POST)
 	public String create(@PathVariable int eventId,
-			@Valid TicketViewModel viewModel, BindingResult result, Model model) {
+			@Valid TicketViewModel viewmodel, BindingResult result, Model model) {
 		if (result.hasErrors()) {  	        
 			model.addAttribute("error", result.getAllErrors());
-			model.addAttribute("ticket", viewModel);
+			model.addAttribute("ticket", viewmodel);
 			model.addAttribute("currentPage", "TicketAdd.jsp");
 			return "MainFrame";
 		}
 
-		viewModel.setEvent(new Event(eventId));
-		Ticket ticket = TicketConverter.convert(viewModel, datamanager);
+		viewmodel.setEvent(new Event(eventId));
+		Ticket ticket = TicketConverter.convert(viewmodel, datamanager);
 		datamanager.saveTicket(ticket); // TODO What if save failed ?
 		return "redirect:/event/list";
 	}
---
+++ GLO-4003-B5/src/main/java/ca/ulaval/ticketmaster/web/viewmodels/EventViewModel.java
@@ -0,0 +1,66 @@
+package ca.ulaval.ticketmaster.web.viewmodels;
+
+import java.util.Date;
+
+public class EventViewModel {
+	
+	public enum Sport {
+		Football, Basketball, Rugby, Soccer, Volleyball
+	}
+
+	//public Sport sport;
+	public String gender;
+	public String homeTeam;
+	public String visitorsTeam;
+	public String location;
+	public String stadium;
+	public Date date;
+	public Date time;
+	//public List<Ticket> ticketList;
+	//public List<String> sectionList;
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getHomeTeam() {
+		return homeTeam;
+	}
+	public void setHomeTeam(String homeTeam) {
+		this.homeTeam = homeTeam;
+	}
+	public String getVisitorsTeam() {
+		return visitorsTeam;
+	}
+	public void setVisitorsTeam(String visitorsTeam) {
+		this.visitorsTeam = visitorsTeam;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getStadium() {
+		return stadium;
+	}
+	public void setStadium(String stadium) {
+		this.stadium = stadium;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	
+	
+}
