ddc3dbdc99b1ec86e13de9f2153f9106c995b371
TheMadSword <bdetdbigfiles@gmail.com>
2013-10-07 03:22:55 -0400

    HomeController un peu mieu @ Tests; mais le test du login = wtf (Session HTTP se fait pas set ses attributes ?)

--- GLO-4003-B5/src/main/java/ca/ulaval/ticketmaster/web/HomeController.java
+++ GLO-4003-B5/src/main/java/ca/ulaval/ticketmaster/web/HomeController.java
@@ -100,10 +100,7 @@ public class HomeController {
 	if (!userIsOk) {
 	    model.addAttribute("errorMsg", "La combinaison pseudo/mot de passe est invalide");
 	    model.addAttribute("currentPage", "Home.jsp");
-	    //return "Mainframe";
-	} else {
-	    //return "Home";
-	}
+	} 
 	
 	return "Home";
 	
--- GLO-4003-B5/src/test/java/ca/ulaval/ticketmaster/web/HomeControllerTest.java
+++ GLO-4003-B5/src/test/java/ca/ulaval/ticketmaster/web/HomeControllerTest.java
@@ -3,16 +3,29 @@ package ca.ulaval.ticketmaster.web;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpSession;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.validation.support.BindingAwareModelMap;
+//import org.springframework.mock.web.MockHttpSession;
+
+
+
+
+
 
 import ca.ulaval.ticketmaster.dao.util.DataManager;
+import ca.ulaval.ticketmaster.dao.util.TicketFactory;
+import ca.ulaval.ticketmaster.model.User;
+import ca.ulaval.ticketmaster.model.User.AccessLevel;
 import ca.ulaval.ticketmaster.web.viewmodels.UserViewModel;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -69,7 +82,7 @@ public class HomeControllerTest {
     }
 
     @Test
-    public void testLogin() {
+    public void testLoginGET() {
 
 	String ret = controller.Login(model);
 
@@ -78,15 +91,97 @@ public class HomeControllerTest {
     }
 
     @Test
+    public void testSuccessfulLogin() {
+
+	HttpSession httpSession = Mockito.mock( HttpSession.class );
+	
+	String adminUsername = "CarloBoutet";
+	
+	User admin = new User(adminUsername);
+	admin.setPassword("123");
+	admin.setAccessLevel(AccessLevel.Admin);
+	
+	when(datamanager.findUser(adminUsername)).thenReturn(admin);
+	
+	//FOQQWEQEFQOF
+	//when(httpSession.setAttribute("sesusername", admin.getAccessLevel().toString()).thenReturn("bob"));
+
+	String ret = controller.Login(adminUsername, admin.getPassword(), model, httpSession);
+
+	assertEquals(ret, "Home");
+
+	assertNotNull(httpSession.getAttribute("sesusername"));
+	assertNotNull(httpSession.getAttribute("sesacceslevel"));
+
+	//assertNotNull(model.get("sesacceslevel"));
+	//assertNotNull(model.get("sesusername"));
+
+    }
+
+    @Test
+    public void testFailLoginWithExistingUser() {
+
+	HttpSession httpSession = Mockito.mock( HttpSession.class );
+	
+	User admin = new User("CarloBoutet");
+	admin.setPassword("123");
+	
+	when(datamanager.findUser(admin.getUsername())).thenReturn(admin);
+
+	String ret = controller.Login(admin.getUsername(), "NOTTHEPASSWORD", model, httpSession);
+
+	assertEquals(ret, "Home");
+
+	assertNull(httpSession.getAttribute("sesacceslevel"));
+	assertNull(httpSession.getAttribute("sesusername"));
+
+	//On ne verifie pas le contenu car ce serait restrictif si on change un message d'erreur
+	//@TODO demander au prof
+	assertNotNull(model.get("errorMsg"));
+	assertNotNull(model.get("currentPage"));
+
+    }
+
+    @Test
+    public void testFailLoginWithUnexistingUser() {
+
+	HttpSession httpSession = Mockito.mock( HttpSession.class );
+
+	String ret = controller.Login("HEY!!", "FFFFFFUUUUUUUUU", model, httpSession);
+
+	assertEquals(ret, "Home");
+
+	assertNull(httpSession.getAttribute("sesacceslevel"));
+	assertNull(httpSession.getAttribute("sesusername"));
+
+	//On ne verifie pas le contenu car ce serait restrictif si on change un message d'erreur
+	//@TODO demander au prof
+	assertNotNull(model.get("errorMsg"));
+	assertNotNull(model.get("currentPage"));
+
+    }
+
+    @Test
     public void testDisconnect() {
 
-	String ret = controller.Disconnect(model, /* ehm... */null);
+	HttpSession httpSession = Mockito.mock( HttpSession.class );
+    	
+	String ret = controller.Disconnect(model, /* ehm... null*/httpSession);
+	
+	assertNull(httpSession.getAttribute("sesacceslevel"));
+	assertNull(httpSession.getAttribute("sesusername"));
 
-	assertNull(model.get("sesacceslevel"));
-	assertNull(model.get("sesusername"));
+	assertEquals(ret, "Home");
 
-	assertEquals(ret, "MainFrame");
+    }
+    
+    @Test
+    public void testBasket() {
+    	String ret = controller.Basket(model);
+    	
+    	assertEquals(model.get("currentPage"), "Basket.jsp");
 
+    	assertEquals(ret, "Basket");
     }
 
 }
