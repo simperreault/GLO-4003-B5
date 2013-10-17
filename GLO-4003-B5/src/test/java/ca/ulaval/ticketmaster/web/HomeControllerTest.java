d993e9d85dc5638bf8d0f42637a9d1bb362da6ee
TheMadSword <bdetdbigfiles@gmail.com>
2013-10-17 18:10:04 -0400

    Test HomeController (incomplet; mais wtf le 2e "niveau" amener)

--- GLO-4003-B5/src/test/java/ca/ulaval/ticketmaster/web/HomeControllerTest.java
+++ GLO-4003-B5/src/test/java/ca/ulaval/ticketmaster/web/HomeControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 
 import javax.servlet.http.HttpSession;
 
@@ -13,14 +14,21 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.support.BindingAwareModelMap;
 //import org.springframework.mock.web.MockHttpSession;
 import org.springframework.mock.web.MockHttpSession;
 
 
+
+
+
+
 import ca.ulaval.ticketmaster.dao.util.DataManager;
 import ca.ulaval.ticketmaster.model.User;
 import ca.ulaval.ticketmaster.model.User.AccessLevel;
+import ca.ulaval.ticketmaster.web.DomaineAffaire.DAUser;
+import ca.ulaval.ticketmaster.web.converter.UserConverter;
 import ca.ulaval.ticketmaster.web.viewmodels.UserViewModel;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -28,6 +36,9 @@ public class HomeControllerTest {
 
     @Mock
     public DataManager datamanager;
+    
+    @InjectMocks
+	public DAUser domaine;
 
     @InjectMocks
     public HomeController controller;
@@ -65,16 +76,24 @@ public class HomeControllerTest {
 
     @Test
     public void testCreateUserPOST() {
+    	//Je test directement dans le domaine, puisque le controlleur ne fait qu'un appel .. ?
     	
     	HttpSession httpSession = new MockHttpSession();
     	
 	UserViewModel viewModel = new UserViewModel();
 	viewModel.setUsername("bob");
+	
+    BindingResult result = mock(BindingResult.class);
+    
+    User user = UserConverter.convert(viewModel);
+	
+	when(datamanager.saveUser(user)).thenReturn(true);
 
-	String ret = controller.CreateUser(viewModel, null, model, httpSession);
+	String ret = domaine.createUser(user, viewModel, model, result, httpSession);
 
 
-	assertEquals(model.get("username"), viewModel.getUsername());
+	//assertEquals(model.get("sesusername"), viewModel.getUsername());
+	assertEquals(httpSession.getAttribute("sesusername"), user.getUsername());
 	assertEquals(ret, "Home");
 
     }
