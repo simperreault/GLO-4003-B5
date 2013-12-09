655623174dfb7b1cf5b562e5359027782fd0f39f
Rhita OULIZ <rhita.ouliz.1@ulaval.ca>
2013-12-09 13:29:01 -0500

    pushed new test

---
+++ GLO-4003-B5/src/test/java/ca/ulaval/ticketmaster/events/tickets/DATicketTest.java
@@ -0,0 +1,23 @@
+package ca.ulaval.ticketmaster.web.DomaineAffaire;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import ca.ulaval.ticketmaster.dao.util.DataManager;
+@RunWith(MockitoJUnitRunner.class)
+
+public class DATicketTest {
+
+	@Mock
+    public DataManager datamanager;
+	
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
